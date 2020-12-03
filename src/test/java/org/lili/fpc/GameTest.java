8cadb5a749c64daa93947511066e5c43093ac7e9
lili <xiaozhiliaoo@gmail.com>
2020-12-04 02:48:22 +0800

    up

---
+++ src/test/java/org/lili/fpc/GameTest.java
@@ -0,0 +1,18 @@
+package org.lili.fpc;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+/**
+ * @author lili
+ * @date 2020/12/4 12:25
+ * @notes
+ */
+class GameTest {
+
+    @Test
+    void run() {
+        Assertions.assertTrue(true);
+    }
+}
